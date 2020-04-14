59b6f0b6923e34f3ac2b48b9462a2393feaaccdb
Petoovee <petomatick@gmail.com>
2020-04-14 17:42:52 +0200

    Working Hello World

---
+++ UpgraderSubModule.cs
@@ -0,0 +1,15 @@
+﻿using TaleWorlds.Core;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
+
+namespace Upgrader
+{
+    public class UpgraderSubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            Module.CurrentModule.AddInitialStateOption(
+                new InitialStateOption("Message", new TextObject("Message", null), 9990, () => { InformationManager.DisplayMessage(new InformationMessage("Hello World!")); }, false));
+        }
+    }
+}
